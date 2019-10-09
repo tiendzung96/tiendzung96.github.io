69766f1ece443fc91361d66e46541222eeeaa0d8
tiendzung96 <pha13003@byui.edu>
2019-10-09 01:48:28 -0600

    script

--- cit230/lesson4/scripts/currentDate.js
+++ cit230/lesson4/scripts/currentDate.js
@@ -3,4 +3,4 @@ var currentYear = currentDate.getFullYear();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-document.getElementById("currentYear").innerHTML = days[currentDate.getDay()] + ", " + months[currentDate.getMonth()]+ ", " + currentYear;
+document.getElementById("currentYear").innerHTML = days[currentDate.getDay()] + ", " + months[currentDate.getMonth()]+ " " + currentDate.getDate() + ", " + currentYear;
