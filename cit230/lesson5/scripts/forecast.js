5ec0e3a824bc5bfd6b156802444619646cf80eb5
tiendzung96 <pha13003@byui.edu>
2019-10-16 10:12:55 -0600

    update

---
+++ cit230/lesson5/scripts/forecast.js
@@ -0,0 +1,11 @@
+/* GET DATE */
+var currentDate = new Date();
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+document.getElementById("day1").innerHTML = days[currentDate.getDay()];
+document.getElementById("day2").innerHTML = days[currentDate.getDay()+1];
+document.getElementById("day3").innerHTML = days[currentDate.getDay()+2];
+document.getElementById("day4").innerHTML = days[currentDate.getDay()+3];
+document.getElementById("day5").innerHTML = days[currentDate.getDay()+4];
+
+/* GET TEMPERATURE */
