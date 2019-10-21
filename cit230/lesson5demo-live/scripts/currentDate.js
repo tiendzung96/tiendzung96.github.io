540f1fc81abcb5069b91f12cdbe80af60a854b43
tiendzung96 <pha13003@byui.edu>
2019-10-21 15:39:39 -0600

    live hero image

--- cit230/lesson5demo-live/scripts/currentDate.js
+++
@@ -1,6 +0,0 @@
-let currentDate = new Date();
-let currentYear = currentDate.getFullYear();
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-document.getElementById("currentYear").innerHTML = days[currentDate.getDay()] + ", " + months[currentDate.getMonth()]+ " " + currentDate.getDate() + ", " + currentYear;
