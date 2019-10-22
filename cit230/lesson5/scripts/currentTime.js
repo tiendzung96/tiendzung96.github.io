db1930a486696afdca2cd87bf6f199839896a38f
tiendzung96 <pha13003@byui.edu>
2019-10-22 08:39:20 -0600

    time

--- cit230/lesson5/scripts/currentTime.js
+++ cit230/lesson5/scripts/currentTime.js
@@ -5,7 +5,7 @@ function showCurrentTime(){
     var s = time.getSeconds();
     
     function twoDigits(){
-    h = (h < 10) ?  "0" + h : h;
+    // h = (h < 10) ?  "0" + h : h;
     m = (m < 10) ?  "0" + m : m;
     s = (s < 10) ?  "0" + s : s;
     }
