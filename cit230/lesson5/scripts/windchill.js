523a5b815c477c9f69f36d51650939cdd4f3e959
tiendzung96 <tiendzung96@gmail.com>
2019-10-23 01:39:37 -0600

    chill

--- cit230/lesson5/scripts/windchill.js
+++ cit230/lesson5/scripts/windchill.js
@@ -6,10 +6,13 @@ var weather = {
     windSpeed: 5
 };
 
-console.log(weather);
+let t = (weather.low + weather.high)/2;
+let s = weather.windSpeed;
+weather.windChill = Math.floor(35.74 + 0.6215*t - 35.75*Math.pow(s, 0.16)  + 0.4275*t*Math.pow(s, 0.16));
 
 document.getElementById('current-weather').innerHTML = '<b>Currently:</b> ' + weather.currentWeather;
 document.getElementById('high').innerHTML = '<b>High:</b> ' + weather.high + '&deg;F';
 document.getElementById('low').innerHTML = '<b>Low:</b> ' + weather.low + '&deg;F';
-document.getElementById('wind-chill').innerHTML = '<b>Wind Chill:</b> ' + weather.low + '&deg;F';
+document.getElementById('wind-chill').innerHTML = '<b>Wind Chill:</b> ' + weather.windChill + '&deg;F';
 document.getElementById('wind-speed').innerHTML = '<b>Wind Speed:</b> ' + weather.windSpeed + ' mph';
+
