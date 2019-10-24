25cd592a603251e067075c7ef2ba5a1c0a38ae42
tiendzung96 <tiendzung96@gmail.com>
2019-10-24 13:34:13 -0600

    weather

--- cit230/lesson5/scripts/forecast.js
+++ cit230/lesson5/scripts/forecast.js
@@ -3,11 +3,30 @@ function showCurrentDate(){
     let currentDate = new Date();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    document.getElementById("day1").innerHTML = days[currentDate.getDay()+1];
-    document.getElementById("day2").innerHTML = days[currentDate.getDay()+2];
-    document.getElementById("day3").innerHTML = days[currentDate.getDay()+3];
-    document.getElementById("day4").innerHTML = days[currentDate.getDay()+4];
-    document.getElementById("day5").innerHTML = days[currentDate.getDay()+5];
+    document.getElementById("day-1").innerHTML = days[currentDate.getDay()+1];
+    document.getElementById("day-2").innerHTML = days[currentDate.getDay()+2];
+    document.getElementById("day-3").innerHTML = days[currentDate.getDay()+3];
+    document.getElementById("day-4").innerHTML = days[currentDate.getDay()+4];
+    document.getElementById("day-5").innerHTML = days[currentDate.getDay()+5];
 }
 
 showCurrentDate();
+
+
+/* GET TEMP */
+
+const forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?zip=83263,us&units=imperial&appid=9479da382fe4e6454b2b21f3e81c03d7';
+fetch(forecastURL)
+  .then((responseF) => responseF.json())
+  .then((forecastObject) => {
+    // console.log(forecastObject);
+    
+    for (let i=1; i<6; i++){
+        document.getElementById("temp-"+ i).innerHTML = Math.ceil(forecastObject.list[i].main.temp) + '&deg;F';
+    }
+});
+
+
+// https://tile.openweathermap.org/map/temp_new/190.14/43.5667/-116.2410.png?appid=9479da382fe4e6454b2b21f3e81c03d7
+
+//https://weather.api.here.com/weather/1.0/report.xml?app_id=n4BB2dFEjCniatJtSnoG&app_code=cphQnNWEkkaVce1m6fotKA&product=observation&name=Idaho
--- cit230/lesson5/scripts/weatherData.js
+++ cit230/lesson5/scripts/weatherData.js
@@ -1,34 +1,41 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?zip=83263,us&appid=9479da382fe4e6454b2b21f3e81c03d7';
-fetch(apiURL)
+const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?zip=83263,us&units=imperial&appid=9479da382fe4e6454b2b21f3e81c03d7';
+fetch(weatherURL)
   .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    console.log(jsObject.weather[0].main);
-  
+  .then((weatherObject) => {
+    console.log(weatherObject);
 
+    let weather = {};
 
-var weather = {
-    currentTemp: 30,
-    high: 50,
-    low: 20,
-    humitity: 50,
-    windSpeed: 5
-};
+    weather.currentWeather = weatherObject.weather[0].main;
+    weather.currentTemp = Math.ceil(weatherObject.main.temp);
+    weather.high = Math.ceil(weatherObject.main.temp_max);
+    weather.low = Math.ceil(weatherObject.main.temp_min);
+    weather.humidity = weatherObject.main.humidity;
+    weather.windSpeed = weatherObject.wind.speed;
 
-weather.currentWeather = jsObject.weather[0].main;
+//WEATHER ICON
 
-let t = weather.currentTemp;
-let s = weather.windSpeed;
-if(weather.low <= 50 && s > 3){
-    weather.windChill = Math.floor(35.74 + 0.6215*t - 35.75*Math.pow(s, 0.16)  + 0.4275*t*Math.pow(s, 0.16)) + '&deg;F';
-} else{
-    weather.windChill = 'N/A';
-}
+    const imagesrc = 'https://openweathermap.org/img/w/' + weatherObject.weather[0].icon + '.png';  
+    const desc = weatherObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('weather-icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('weather-icon').setAttribute('alt', desc);
 
-document.getElementById('current-weather').innerHTML = '<b>Currently:</b> ' + weather.currentWeather;
-document.getElementById('high').innerHTML = '<b>High:</b> ' + weather.high + '&deg;F';
-document.getElementById('low').innerHTML = '<b>Low:</b> ' + weather.low + '&deg;F';
-document.getElementById('wind-chill').innerHTML = '<b>Wind Chill:</b> ' + weather.windChill;
-document.getElementById('wind-speed').innerHTML = '<b>Wind Speed:</b> ' + weather.windSpeed + ' mph';
+    // console.log(weather);
+
+    let t = weather.currentTemp;
+    let s = weather.windSpeed;
+    if(weather.low <= 50 && s > 3){
+        weather.windChill = Math.ceil(35.74 + 0.6215*t - 35.75*Math.pow(s, 0.16)  + 0.4275*t*Math.pow(s, 0.16)) + '&deg;F';
+    } else{
+        weather.windChill = 'N/A';
+    }
+
+    document.getElementById('current-weather').innerHTML = '<b>Currently:</b> ' + weather.currentWeather;
+    document.getElementById('current-temp').innerHTML = '<b>Temperature:</b> ' + weather.currentTemp  + '&deg;F';
+    document.getElementById('high').innerHTML = '<b>High:</b> ' + weather.high + '&deg;F';
+    document.getElementById('low').innerHTML = '<b>Low:</b> ' + weather.low + '&deg;F';
+    document.getElementById('humidity').innerHTML = '<b>Humidity:</b> ' + weather.humidity + ' %';
+    document.getElementById('wind-chill').innerHTML = '<b>Wind Chill:</b> ' + weather.windChill;
+    document.getElementById('wind-speed').innerHTML = '<b>Wind Speed:</b> ' + weather.windSpeed + ' mph';
 
 });
