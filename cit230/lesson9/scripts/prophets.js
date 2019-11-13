4df37bdc2a1782a84d785d11b3de291bc9b1a95d
tiendzung96 <tiendzung96@gmail.com>
2019-11-13 10:44:51 -0700

    prophets

--- cit230/lesson9/scripts/prophets.js
+++ cit230/lesson9/scripts/prophets.js
@@ -5,31 +5,56 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     // console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
+    const prophets = jsonObject.prophets;
 
 
-    for (let i = 0; i < prophets.length; i++ ) {
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      card.appendChild(h2);
+    // for (let i = 0; i < prophets.length; i++ ) {
+    //   let card = document.createElement('section');
+    //   let h2 = document.createElement('h2');
+    //   h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+    //   card.appendChild(h2);
 
-      let birthdate  = document.createElement('p');
-      birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-      card.appendChild(birthdate);
+    //   let birthdate  = document.createElement('p');
+    //   birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+    //   card.appendChild(birthdate);
 
-      let birthplace  = document.createElement('p');
-      birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-      card.appendChild(birthplace);
+    //   let birthplace  = document.createElement('p');
+    //   birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+    //   card.appendChild(birthplace);
 
-      let image = document.createElement('img');
-      image.setAttribute('src', prophets[i].imageurl);
-      image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname +' - '+prophets[i].length);
-      card.appendChild(image);
+    //   let image = document.createElement('img');
+    //   image.setAttribute('src', prophets[i].imageurl);
+    //   image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname +' - '+prophets[i].length);
+    //   card.appendChild(image);
 
-      document.querySelector('div.cards').appendChild(card);
+    //   document.querySelector('div.cards').appendChild(card);
+    // }
 
-    }
+    prophets.forEach(
+      (prophet) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        
+        h2.textContent = prophet.name + ' ' + prophet.lastname;
+        card.appendChild(h2);
+
+        let birthdate  = document.createElement('p');
+        birthdate.textContent = 'Date of Birth: ' + prophet.birthdate;
+        card.appendChild(birthdate);
+
+        let birthplace  = document.createElement('p');
+        birthplace.textContent = 'Place of Birth: ' + prophet.birthplace;
+        card.appendChild(birthplace);
+
+        let image = document.createElement('img');
+        image.setAttribute('src', prophet.imageurl);
+        image.setAttribute('alt', prophet.name + ' ' + prophet.lastname +' - '+prophet.length);
+        card.appendChild(image);
+
+        document.querySelector('div.cards').appendChild(card);
+      }
+    );
+
   });
 
   WebFont.load({
