6e71e2bd719f167efe0929b3a2d614f29051fc7c
tiendzung96 <tiendzung96@gmail.com>
2019-11-15 14:10:58 -0700

    better function

--- cit230/lesson9/scripts/town-data.js
+++ cit230/lesson9/scripts/town-data.js
@@ -1,31 +1,35 @@
 const townURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
 fetch(townURL)
   .then(response => {
       response.json()
       .then(
           response => {
-                const towns = response.towns;
-                console.log(response.towns);
+            const towns = response.towns;
 
-                let fishHaven = response.towns[1];
-                let fishHavenName = 'fishHaven';
-                getTownData(fishHaven, fishHavenName);  
+            const fishHaven = getTownObject('Fish Haven');
+            const fishHavenName = 'fishHaven';
+            getTownData(fishHaven, fishHavenName);  
 
-                let preston = response.towns[4];
-                let prestonName = 'preston';
-                getTownData(preston, prestonName);  
+            const preston = getTownObject('Preston');
+            const prestonName = 'preston';
+            getTownData(preston, prestonName);  
 
-                let sodaSprings = response.towns[5];
-                let sodaSpringsName = 'sodaSprings';
-                getTownData(sodaSprings, sodaSpringsName);  
+            let sodaSprings = getTownObject('Soda Springs');
+            let sodaSpringsName = 'sodaSprings';
+            getTownData(sodaSprings, sodaSpringsName);  
 
-                function getTownData (town, townName) {
-                    document.querySelector('#'+ townName + 'Img').setAttribute('src', 'images/town/edited/' + town.photo);
-                    document.querySelector('#'+ townName + 'Motto').textContent = '"'+town.motto+'"';
-                    document.querySelector('#'+ townName + 'Year').textContent = town.yearFounded;
-                    document.querySelector('#'+townName+'Population').textContent = town.currentPopulation;
-                    document.querySelector('#'+townName+'Rain').textContent = town.averageRainfall;
-                }
+            function getTownObject(name){
+                return towns.find(element => element.name === name);
+            }
+            
+            function getTownData (town, townName) {
+                document.querySelector('#'+ townName + 'Img').setAttribute('src', 'images/town/edited/' + town.photo);
+                document.querySelector('#'+ townName + 'Motto').textContent = '"'+town.motto+'"';
+                document.querySelector('#'+ townName + 'Year').textContent = town.yearFounded;
+                document.querySelector('#'+townName+'Population').textContent = town.currentPopulation;
+                document.querySelector('#'+townName+'Rain').textContent = town.averageRainfall;
+            }
           }
       )
   });
