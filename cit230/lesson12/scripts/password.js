a3ca4b1538ef8b64af78adf167b2ca024c9a3167
tiendzung96 <tiendzung96@gmail.com>
2020-01-09 10:58:58 -0700

    password

---
+++ cit230/lesson12/scripts/password.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="19011996";
+
+password=prompt('What\'s Douglas birthday: ddmmyyyy?');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://tiendzung96.github.io/";
+    }
